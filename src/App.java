2b725a6902870040678ba8dc183896a0e77fdba2
yanawut pomkaew <AnathamPK@gmail.com>
2023-06-23 08:48:00 +0700

    Add Student Class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        ArrayList<String> students = new ArrayList<String>();
+        
+        Student student = new Student();
+
+        student.setName("Yanawut");
+        System.out.println(student.getName());
+
+        students.add(student.getName());
+
+        student.setName("ice");
+        System.out.println(student.getName());
+        students.add(student.getName());
+        
+        
+        for(int i = 0 ; i< students.size() ; ++i) {
+            System.out.print(students.get(i) + ", ");
+            
+        }
+
     }
 }
